a39f019b540b59e198dcec54860082aa57a12c49
DAVID JIMENEZ <davidinformatica18@gmail.com>
2026-01-13 11:45:27 +0100

    Refactor FizzBuzz logic and add input validation
    Added error handling for non-integer input and corrected indentation for conditional statements.

--- SolicitudUsuarioNuevo.py
+++ SolicitudUsuarioNuevo.py
@@ -4,24 +4,27 @@
 # - Divisible entre 3 y entre 5, el programa debe imprimir "FizzBuzz"
 # - Si no es divisible ni entre 3 ni entre 5, debe imprimir el número
 
-numero = int(input("Escribe un número: ")) # La variable con el nombre numero guarda el valor int y input que pregunta el numero al usuario
+try:
+
+    numero = int(input("Escribe un número: ")) # La variable con el nombre numero guarda el valor int y input que pregunta el numero al usuario
 
 
-if numero % 3 == 0 and numero % 5 == 0:    # Condicional if dice que numero es divise entre 3 y es igual a 0 y nuevo numero es divise entre 5 y es igual a 0
+    if numero % 3 == 0 and numero % 5 == 0:    # Condicional if dice que numero es divise entre 3 y es igual a 0 y nuevo numero es divise entre 5 y es igual a 0
 
-    print ("FizzBuzz")                         # Imprime FizzBuzz
+     print ("FizzBuzz")                         # Imprime FizzBuzz
 
-elif numero % 3 == 0:                      # Condicional elif (y si no) dice que numero es divise entre 3 y es igual a 0
+    elif numero % 3 == 0:                      # Condicional elif (y si no) dice que numero es divise entre 3 y es igual a 0
+        print ("Fizz")                         # Imprime Fizz
 
-    print ("Fizz")                         # Imprime Fizz
+    elif numero % 5 == 0:                      # Condicional elif (y si no) dice que numero es divise entre 5 y es igual a 0
 
-elif numero % 5 == 0:                      # Condicional elif (y si no) dice que numero es divise entre 5 y es igual a 0
+        print ("Buzz")                         # Imprime Buzz
+    else:                                      # ESi no se cumple ninguna de las condiciones anteriores
 
-    print ("Buzz")                         # Imprime Buzz
+        print (numero)
 
-else:                                      # ESi no se cumple ninguna de las condiciones anteriores
-
-    print (numero)
+except ValueError:                                 # Excepcion para el caso que el usuario no ingrese un numero valido
+    print("Por favor, ingresa un número válido.")  # Imprime mensaje de error
 
 
 
@@ -38,5 +41,7 @@ def nombre_usuario():
     else:
         
         print ("Usuario no admitido Fizz")
+
+    nombre_usuario()
 
     nombre_usuario()
