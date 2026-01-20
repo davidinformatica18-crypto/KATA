ac845a4b3889bf414572e767fc2871026fe2af65
DAVID JIMENEZ <davidinformatica18@gmail.com>
2026-01-20 11:16:34 +0100

    Refactor test functions in Ejercicio_FizzBuzz_Assert.py
    Refactor test functions to improve structure and readability. Removed unnecessary comments and pass statements.

--- Ejercicio_FizzBuzz_Assert.py
+++ Ejercicio_FizzBuzz_Assert.py
@@ -1,64 +1,66 @@
+# Ejercicio_FizzBuzz_Assert.py
 
-# ESTE TEST VA SUMAR DOS NUMEROS _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+from funciones import *
 
-def test_suma():                                    
+# TESTS _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+
+
+def test_suma():
+   
     assert suma(2, 3) == 5
 
-# ESTE TEST VA RESTART DOS NUMEROS _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-
 def test_resta():
+    
     assert resta(5, 3) == 2
-    pass
 
-# ESTE TEST VA RESTART DOS NUMEROS _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-
-def test_resta():
-    assert resta(5, 3) == 2
-    pass    
-
-# ESTE TEST VA MULTIPLICAR DOS NUMEROS _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 
 def test_multiplicacion():
+  
     assert multiplicacion(2, 3) == 6
-    pass
 
-
-# ESTE TEST VA DIVIDIR DOS NUMEROS _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 
 def test_division():
+    
     assert division(6, 2) == 3
-    pass
 
- # ESTE TEST VA A COMPROBAR LA FUNCION FIZZBUZZ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+# TEST FIZZBUZZ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
 
 def test_fizzbuzz():
-    assert fizzbuzz(15) == "FizzBuzz"
-    assert fizzbuzz(9) == "Fizz"
-    pass
 
-# ESTE TEST VA A COMPROBAR LA FUNCION NOMBRE_USUARIO _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+    assert fizzbuzz(15) == "FizzBuzz"
+
+    assert fizzbuzz(9) == "Fizz"
+
+# TEST USUARIO _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
 
 def test_nombre_usuario():
-    assert nombre_usuario("pablo") == "Bienbenido Pablo, FizzBuzz"
-    assert nombre_usuario("juan") == "Usuario no admitido Fizz"
-    pass   
 
-# ESTE TEST VA A COMPROBAR LA FUNCION EDAD_USUARIO _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+    assert nombre_usuario("pablo") == "Bienbenido Pablo, FizzBuzz"
+
+    assert nombre_usuario("juan") == "Usuario no admitido Fizz"
+
 
 def test_edad_usuario():
-    assert edad_usuario(2000) == 26
-    assert edad_usuario(1990) == 36
-    pass
 
- # ESTE TEST VA A COMPROBAR LA FUNCION PAR_IMPAR _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+    assert edad_usuario(2000) == 26 # Si nació en 2000 tiene 26 años
+
+    assert edad_usuario(1990) == 36 # Si nació en 1990 tiene 36 años
+
+# TEST PAR / IMPAR _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 
 def test_par_impar():
-    assert par_impar(4) == "par"
-    assert par_impar(5) == "impar"
-    pass
+  
+    assert par_impar(4) == "par"  # 4 es par
 
- # ESTE TEST VA A COMPROBAR LA FUNCION FRUTAS_M _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+    assert par_impar(5) == "impar" # 5 es impar
+
+# TEST FRUTAS _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
 
 def test_frutas_m():
-    assert frutas_m(["manzana", "pera", "mango", "melón", "uva"]) == ["manzana", "mango", "melón"]
-    pass
+
+    frutas = ["manzana", "pera", "mango", "melón", "uva"]
+
+    assert frutas_m(frutas) == ["manzana", "mango", "melón"] # Solo tienen que salir las que empiezan por m _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
